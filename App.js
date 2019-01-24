a21b680cb61c5d85d01a3664241190eeebca0c54
Mosias <rafael.mosias2@gmail.com>
2019-01-24 13:58:59 -0200

    Estruturado componentes Post, Author, Header, Comments

--- App.js
+++ App.js
@@ -2,12 +2,16 @@ import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 
 import Header from './src/components/Header'
+import Post from './src/components/Post'
 
 
 export default class App extends Component{
   render() {
     return (
+        <View style={{flex:1}}>
           <Header/>
+          <Post image={require('./assets/imgs/fence.jpg')} />
+        </View>
     );
   }
 }
---
+++ src/components/Comments.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+import {
+    View,
+    Text,
+    StyleSheet,
+    Dimensions,
+    Alert
+} from 'react-native'
+
+class Comments extends Component{
+    render(){
+        let view = null
+    }
+}
