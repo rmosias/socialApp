59a65a1f9c3c83019f98d00d1635642c0d3fb780
Mosias <rafael.mosias2@gmail.com>
2019-01-24 12:44:27 -0200

    Created Header component(first commit)

---
+++ App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+
+import Header from './src/components/Header'
+
+
+export default class App extends Component{
+  render() {
+    return (
+          <Header/>
+    );
+  }
+}
+
---
+++ src/components/Post.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+import {Stylesheet, View, Image, Dimensions} from 'react-native'
+
+class Post extends Component {
+    render(){
+        return(
+            <View style={styles.container}>
+            </View>
+        )
+    }
+}
