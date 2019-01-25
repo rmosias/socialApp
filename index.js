8e049bf43e4b523463121a4466ec5c4668b5f707
Mosias <rafael.mosias2@gmail.com>
2019-01-25 14:02:24 -0200

    Tela de Feed

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-/** @format */
-
 import {AppRegistry} from 'react-native';
-import App from './App';
+import Navigator from './src/Navigator'
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Navigator);
